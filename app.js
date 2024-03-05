f4bb108682f4940c7de8a0d1f9516bb2f4164b70
hasnain2244 <154471487+hasnain2244@users.noreply.github.com>
2024-03-05 13:39:06 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,63 @@
-var|= username
-alert("Hello! I am an alert box!!");
-let carName1 = "Volvo XC60";
+
+//**************** Chapter 2 ***********************//
+
+ 
+//Question 1. Declare a variable called username.
+
+let username;
+
+//Question 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+
+    let myName = "Ghulam Mustafa";
+
+//Question 3. Write script to
+// a) Declare a JS variable, titled message.
+    let message;
+// b) Assign “Hello World” to variable message
+    message = "Hello World";
+// c) Display the message in alert box.
+    alert(message);
+
+//Question 4 Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+    
+    let stdName = "Ghulam Mustafa";
+    let stdAge = 23;
+    let stdCert = "Certified Web and Mobile Application Development";
+    
+    alert("My name is " + stdName);
+    alert("I am " + stdAge + " years old");
+    alert("I am doing " + stdCert + " from SMIT");
+
+//Question 5 Write a script to display the following alert using one JS variable:
+
+    let _alert="PIZZA\nPIZZ\nPIZ\nPI\nP";
+    alert(_alert);
+
+// Question 6 Declare a variable called email and assign to it a string that represents your Email Address
+// (e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use string concatenation)
+
+    let email = "ghulammuztafapk@gmail.com";
+    alert("My Email Address is " + email);
+
+//Question 7 Declare a variable called book & give it the value “A smarter way to learn JavaScript”. 
+// Display the following message in an alert box:
+
+    let book = '"'+"A smarter way to learn JavaScript"+'"';
+    alert("I am trying to learn javaScript from the Book "+book);
+
+//Question 8 Write a script to display this in browser through JS
+    
+    let browser = "Yah! I can write HTML content through JavaScript";
+    document.write("<h1 style=font-size:100px;>"+browser+"</h1>");
+
+//Question 9 Store following string in a variable and show in alert and browser through JS    
+
+    let str = "\n\n▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬\n\n";
+    alert(str);
+    document.write("<h1 style=font-size:53px;>"+str+"</h1>");
+
+    
+
+//***********************************The End********************************************************//
+
